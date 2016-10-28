5e3041a2928e4fd28ef763da90f34a186aa5d0d2
pmadriana <adriana.perez@ucsp.edu.pe>
2016-10-28 10:05:46 -0500

    Update vector.cpp

--- Pila/vector.cpp
+++ Pila/vector.cpp
@@ -6,11 +6,15 @@ using namespace std;
 main()
 {
     vector <int> a;
-    a.push_back(9);
     a.push_back(8);
     a.push_back(7);
-    a.push_back(2;
+    a.push_back(2);
+    a.push_back(5);
+    a.push_back(3);
+    a.insert(a.begin()+3,10);
     for(int i = 0; i < a.size(); i++)
         cout << a[i] << " ";
-
+    cout<<"El tamano anterior era: "<<a.size()<<endl;
+    a.resize(4);
+    cout<<"El nuevo tamano es: "<<a.size()<<endl;
 }
