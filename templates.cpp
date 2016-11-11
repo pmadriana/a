56cdad8725c863a638365b185f08c08a59333fc1
pmadriana <adriana.perez@ucsp.edu.pe>
2016-11-11 09:51:53 -0500

    Update templates.cpp

--- templates.cpp
+++ templates.cpp
@@ -13,17 +13,18 @@ T temp(T x, T y)
 template <typename T>
 T templ(T x, T y)
 {
-    return (x>y)
-        
+    if(x>y)
+        return y;
+    return x;
+
 }
 main()
 {
-   int a=9; c;
+   int a=9, c;
    int b=8;
-   c= temp(a,b)
-   int x= templ(1,6);
-   cout<<c;
-    
-
+   c= temp(a,b);
+   int x;
+   x=templ(1,6);
+   cout<<c<<"\n";
+   cout<<x;
 }
-
